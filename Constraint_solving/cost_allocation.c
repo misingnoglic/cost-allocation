30f7eb38970b62b4bbe18121b61358cec8d40987
arey0pushpa <leadnewindia@hotmail.com>
2016-04-15 11:43:25 +0530

    complete version

--- Constraint_solving/cost_allocation.c
+++ Constraint_solving/cost_allocation.c
@@ -25,8 +25,8 @@ unsigned int  nondet (){
 struct EdgeBag
  {
    unsigned int num ;
-   unsigned int x;
-   unsigned int y;
+    int x;
+    int  y;
    unsigned int costshare;
 };
 
@@ -41,7 +41,7 @@ int  main()
 
  {    
 
-     _Bool C1;
+     _Bool C1,C2;
     unsigned int  i , j , k, l , mi , alpha ;
     double totald1[n1] , totald2[n2] = {} , irDist1[n1] = {}, irDist2[n2] = {}, costd1[n1]  = {} , costd2[n2] = {};
     double totalCost1[n1] , totalCost2[n2];
@@ -55,12 +55,38 @@ int  main()
     struct Start s;  // create a starting point pair 
     struct Start d;  // create a destination pair
 
-/* Optimal Cost
+    /* Long Input */
+
+    cab1[0].x = 2;
+    cab1[1].x = 1;
+    cab1[2].x = 1;
+    cab1[3].x = 1;
+    
+    cab1[0].y = 2;
+    cab1[1].y = 1;
+    cab1[2].y = 0;
+    cab1[3].y = -1;
+
+    cab2[0].x = 3;
+    cab2[0].x = 3;
+    cab2[0].x = 3;
+  
+
+    cab2[0].y = 1;
+    cab2[1].y = 0;
+    cab2[2].y = -1;
+
+    d.x = 0;
+    d.y = 0;
+
+    /* Optimal Cost
+     *
  *-----------------------------------------
  *
  * */
 
 // For the cab 1 
+
    for ( i = 0; i < n1 ; i++) {
      
        if (i != n1 ) {
@@ -121,7 +147,7 @@ for ( i = 0; i < (n1 - 1); i++) {
          de1 = sqrt((cab1[i].x - cab1[i+1].x) * (cab1[i].x - cab1[i+1].x) + (cab1[i].y - cab1[i].y) *(cab1[i].y - cab1[i + 1].y));
            de2 = sqrt((cab1[i + 1].x - d.x) * (cab1[i + 1].x - d.x) + (cab1[i + 1].y - d.y) * (cab1[i + 1].y - d.y));
            del[i] = sqrt((cab1[i].x - d.x) * (cab1[i].x - d.x) + (cab1[i].y - d.y) * (cab1[i].y - d.y));
-           irDist1[i + 1] = aplha (de1 + de2 - del[i]); 
+           irDist1[i + 1] = alpha * (de1 + de2 - del[i]); 
 
 
    }
@@ -131,14 +157,14 @@ for ( i = 0; i < (n1 - 1); i++) {
            de1 = sqrt((cab2[i].x - cab2[i+1].x) * (cab2[i].x - cab2[i+1].x) + (cab2[i].y - cab2[i].y) *(cab2[i].y - cab2[i + 1].y));
            de2 = sqrt((cab2[i + 1].x - d.x) * (cab2[i + 1].x - d.x) + (cab2[i + 1].y - d.y) * (cab2[i + 1].y - d.y));
            de2l[i] = sqrt((cab2[i].x - d.x) * (cab2[i].x - d.x) + (cab2[i].y - d.y) * (cab2[i].y - d.y));
-           irDist2[i] = aplha (de1 + de2 - de2l[i]); 
+           irDist2[i] = alpha * (de1 + de2 - de2l[i]); 
           
       }
 
 /* IR completes  */
 
-for (i = 0; i < n1 ; i++) {
-    if (i = 0) {
+for (i = 0; i < n1; i++) {
+    if (i == 0) {
         totalCost1[i] = costd1[i];
     }
     else {
@@ -147,7 +173,7 @@ for (i = 0; i < n1 ; i++) {
 }
 
 for (i = 0; i < n2 ; i++) {
-    if (i = 0) {
+    if (i == 0) {
         totalCost2[i] = costd2[i];
     }
     else {
@@ -156,12 +182,17 @@ for (i = 0; i < n2 ; i++) {
 }
 
 C1 = 1;
+
 // Sir Constrints 
 for ( i=0;i<n1;i++) {
-        C1 = C1 && (del[i] >= costd1[i]);
+        C1 = (C1 && (del[i] >= costd1[i]));
 }
- 
-__CPROVER_assert(!(C1) , "Cost distribution exists for the current path");  
+
+for ( i=0;i<n1;i++) {
+    C2 = (C2 && (de2l[i] >= costd2[i])) ;
+ }
+
+__CPROVER_assert(!(C1 && C2) , "Cost distribution exists for the current path");  
  
 }
 
